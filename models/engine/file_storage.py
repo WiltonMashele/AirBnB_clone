77e904c06196fa0f8c9e0e6cffed8dbf5a0cfd53
WiltonMashele <Wiltonmashele@gmail.com>
2023-08-12 12:38:45 -0700

    updating FileStorage to manage correctly serialization and deserialization of all our new classes: Place, State, City, Amenity and Review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -39,6 +39,11 @@ class FileStorage:
         """
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
