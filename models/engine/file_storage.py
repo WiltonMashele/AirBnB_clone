068e1fb933cb67de400ebba02495c3aa3702d31b
WiltonMashele <Wiltonmashele@gmail.com>
2023-08-12 11:10:55 -0700

    updating file_storage.py to manage correctly serialization and deserialization of User.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,6 +38,7 @@ class FileStorage:
         not exist.
         """
         from models.base_model import BaseModel
+        from models.user import User
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
